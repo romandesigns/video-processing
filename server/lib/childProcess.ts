7db091811491dc82645bf2b14b0c45869e820d94
Roman F. <wavystyledev@gmail.com>
2024-09-13 10:31:54 -0400

    trying to save output in the same file path as the original

--- server/lib/childProcess.ts
+++ server/lib/childProcess.ts
@@ -1,21 +1,33 @@
 import fs from "fs";
 import ffmpeg from "fluent-ffmpeg";
+import path from "path";
 
 process.on("message", (payload) => {
   const { tempFilePath, name } = payload;
+
   const endProcess = (endPayload) => {
     const { statusCode, text } = endPayload;
+
     // Remove temp file
     fs.unlink(tempFilePath, (err) => {
       if (err) {
         process.send({ statusCode: 500, text: err.message });
       }
     });
-    // Format response so it fits the api response
+
+    // Send response back to parent process
     process.send({ statusCode, text });
+
     // End process
     process.exit();
   };
+
+  // Determine the output directory by using the directory of the original file
+  const originalFileDir = path.dirname(tempFilePath);
+
+  // Define the output path for the processed video
+  const outputFilePath = path.join(originalFileDir, `compressed_${name}`);
+
   // Process video and send back the result
   ffmpeg(tempFilePath)
     .fps(30)
@@ -26,5 +38,5 @@ process.on("message", (payload) => {
     .on("error", (err) => {
       endProcess({ statusCode: 500, text: err.message });
     })
-    .save(`./temp/${name}`);
+    .save(outputFilePath); // Save to the same directory as the original file
 });
